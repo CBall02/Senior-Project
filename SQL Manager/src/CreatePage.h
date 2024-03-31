89fbf4aee378e52eedecace59c55df60d9916578
eestradaCS <eestrada@ufl.edu>
2024-03-31 18:54:01 -0400

    started working on create table constraints: not functional yet

--- SQL Manager/src/CreatePage.h
+++ SQL Manager/src/CreatePage.h
@@ -2,6 +2,7 @@
 
 #include <QDialog>
 #include <qcombobox.h>
+#include <QCheckBox>
 #include <vector>
 #include "ui_CreatePage.h"
 #include "database.h"
@@ -11,7 +12,7 @@ class CreatePage : public QDialog
 	Q_OBJECT
 
 public:
-	CreatePage(QWidget *parent = nullptr);
+	CreatePage(QWidget* parent = nullptr);
 	~CreatePage();
 
 signals:
@@ -24,10 +25,13 @@ private:
 	int numAttributes = 0;
 	std::vector<QLineEdit*> tableNames;
 	std::vector<QComboBox*> tableTypes;
+	std::vector<QCheckBox*> tableConstraints;
 
 
 private slots:
 	void on_plusButton_clicked();
 	void on_createButton_clicked();
 	void on_minusButton_clicked();
+	void on_primaryKey_clicked();
+	void on_unique_or_notNull_clicked();
 };
