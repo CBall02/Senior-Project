74f9365648f9712373c635966d6bd384a16d11c1
eestradaCS <eestrada@ufl.edu>
2024-04-08 15:49:23 -0400

    worked on select page. not integrated with backend yet

--- SQL Manager/src/SelectPage.cpp
+++ SQL Manager/src/SelectPage.cpp
@@ -37,6 +37,32 @@ void SelectPage::on_tablesComboBox_currentIndexChanged(int index)
         attribute->setText(QString::fromStdString(column.name));
         selections.push_back(attribute);
         newLine->addWidget(attribute);
-        ui.verticalLayout->insertLayout(ui.verticalLayout->count() - 1, newLine);
+        ui.verticalAttributes->insertLayout(ui.verticalAttributes->count() - 1, newLine);
+    }
+}
+
+void SelectPage::on_plusButton_clicked()
+{
+    QHBoxLayout* newLine = new QHBoxLayout();
+    QLabel* WHERE = new QLabel();
+    QLineEdit* condition = new QLineEdit();
+    WHERE->setText("WHERE");
+    newLine->addWidget(WHERE);
+    newLine->addWidget(condition);
+    ui.verticalConditions->insertLayout(ui.verticalConditions->count() - 1, newLine);
+    conditions.push_back(condition);
+    labels.push_back(WHERE);
+    numConditions++;
+}
+
+void SelectPage::on_minusButton_clicked()
+{
+    if (numConditions > 0)
+    {
+        delete conditions.back();
+        conditions.pop_back();
+        delete labels.back();
+        labels.pop_back();
+        delete ui.verticalConditions->takeAt(ui.verticalConditions->count() - 2);
     }
 }
