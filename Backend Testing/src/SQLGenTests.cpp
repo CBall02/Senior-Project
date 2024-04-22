1bd6947ffc36f8ce1db027d822cade796ebf69f6
CBall02 <ball.christian2002@gmail.com>
2024-04-22 12:53:12 -0400

    Add create model and drop model test cases

--- Backend Testing/src/SQLGenTests.cpp
+++ Backend Testing/src/SQLGenTests.cpp
@@ -49,6 +49,170 @@ TEST_CASE("InsertModel", "[InsertModel]") {
 }
 
 
-TEST_CASE("CreateModel", "[CreateModel]") {
 
+TEST_CASE("CreateModel Single Column", "[sqlgenerator::CreateModel]") {
+	sqlGenerator::CreateModel create;
+	create.tableName("CreateModel");
+	REQUIRE(create.str() == "");
+
+
+	SECTION("Single Column int") {
+		create.columns("UFID", "int");
+		REQUIRE(create.str() == "create table CreateModel(UFID int)");
+	}
+
+	SECTION("Single Column int change table name") {
+		create.columns("UFID", "int");
+		create.tableName("ChangedCreateModel");
+		REQUIRE(create.str() == "create table ChangedCreateModel(UFID int)");
+	}
+
+	SECTION("Single Column varchar") {
+		create.columns("UFID", "varchar(255)");
+		REQUIRE(create.str() == "create table CreateModel(UFID varchar(255))");
+	}
+
+	SECTION("Single Column int primary") {
+		create.columns("UFID", "int", true);
+		// not null is implied by primary key, however sqlite does not include this. Explicit for generation
+		REQUIRE(create.str() == "create table CreateModel(UFID int not null, primary key(UFID))");
+	}
+
+	SECTION("Single Column int unique") {
+		create.columns("UFID", "int", false, false, true);
+		REQUIRE(create.str() == "create table CreateModel(UFID int unique)");
+	}
+
+	SECTION("Single Column int not null") {
+		create.columns("UFID", "int", false, true);
+		REQUIRE(create.str() == "create table CreateModel(UFID int not null)");
+	}
+
+	SECTION("Single Column int not null and unique") {
+		create.columns("UFID", "int", false, true, true);
+		REQUIRE(create.str() == "create table CreateModel(UFID int not null unique)");
+	}
+
+	SECTION("Single Column int primary key and unique") {
+		create.columns("UFID", "int", true, false, true);
+		// Primary implies unique so providing it is not required
+		REQUIRE(create.str() == "create table CreateModel(UFID int not null, primary key(UFID))");
+	}
+
+	SECTION("Single Column int not null as foreign key") {
+		create.columns("UFID", "int", false, true, false, {true, "CreateTest", "Column1"});
+		REQUIRE(create.str() == "create table CreateModel(UFID int not null, foreign key (UFID) references CreateTest(Column1))");
+	}
+
+	SECTION("Single Column reset") {
+		create.columns("UFID", "int", false, true, true);
+		REQUIRE(create.str() == "create table CreateModel(UFID int not null unique)");
+		create.reset();
+		REQUIRE(create.str() == "");
+	}
 }
+
+
+
+TEST_CASE("CreateModel Multiple Column", "[sqlgenerator::CreateModel]") {
+	sqlGenerator::CreateModel create;
+	create.tableName("CreateModel");
+	REQUIRE(create.str() == "");
+
+	SECTION("Multiple Column int") {
+		create.columns("UFID", "int");
+		create.columns("SID", "int");
+		REQUIRE(create.str() == "create table CreateModel(UFID int, SID int)");
+	}
+
+	SECTION("Multiple Column int with interleaved addition") {
+		create.columns("UFID", "int");
+		create.columns("SID", "int");
+		REQUIRE(create.str() == "create table CreateModel(UFID int, SID int)");
+		create.columns("FName", "varchar(255)");
+		REQUIRE(create.str() == "create table CreateModel(UFID int, SID int, FName varchar(255))");
+	}
+
+	SECTION("Multiple Column int primary") {
+		create.columns("UFID", "int", true);
+		create.columns("SID", "int");
+		// not null is implied by primary key, however sqlite does not include this. Explicit for generation
+		REQUIRE(create.str() == "create table CreateModel(UFID int not null, SID int, primary key(UFID))");
+	}
+
+	SECTION("Multiple Column int multiple primary") {
+		create.columns("UFID", "int", true);
+		create.columns("SID", "int", true);
+		// not null is implied by primary key, however sqlite does not include this. Explicit for generation
+		REQUIRE(create.str() == "create table CreateModel(UFID int not null, SID int not null, primary key(UFID, SID))");
+	}
+
+	SECTION("Multiple Column int unique") {
+		create.columns("UFID", "int", false, false, true);
+		create.columns("SID", "int");
+		REQUIRE(create.str() == "create table CreateModel(UFID int unique, SID int)");
+	}
+
+	SECTION("Multiple Column int unique") {
+		create.columns("UFID", "int", false, false, true);
+		create.columns("SID", "int", false, false, true);
+		REQUIRE(create.str() == "create table CreateModel(UFID int unique, SID int unique)");
+	}
+
+	SECTION("Multiple Column int not null") {
+		create.columns("UFID", "int", false, true);
+		create.columns("SID", "int");
+		REQUIRE(create.str() == "create table CreateModel(UFID int not null, SID int)");
+	}
+
+	SECTION("Multiple Column int multiple not null") {
+		create.columns("UFID", "int", false, true);
+		create.columns("SID", "int", false, true);
+		REQUIRE(create.str() == "create table CreateModel(UFID int not null, SID int not null)");
+	}
+
+	SECTION("Multiple Column int not null and unique") {
+		create.columns("UFID", "int", false, true, true);
+		create.columns("SID", "int");
+		REQUIRE(create.str() == "create table CreateModel(UFID int not null unique, SID int)");
+	}
+
+	SECTION("Multiple Column int not null and unique") {
+		create.columns("UFID", "int", false, true, true);
+		create.columns("SID", "int", false, true, true);
+		REQUIRE(create.str() == "create table CreateModel(UFID int not null unique, SID int not null unique)");
+	}
+
+	SECTION("Multiple Column int not null as foreign key") {
+		create.columns("UFID", "int", false, true, false, { true, "CreateTest", "Column1" });
+		create.columns("SID", "int", false, true);
+		REQUIRE(create.str() == "create table CreateModel(UFID int not null, SID int not null, foreign key (UFID) references CreateTest(Column1))");
+	}
+
+	SECTION("Multiple Column int not null multiple foreign key") {
+		create.columns("UFID", "int", false, true, false, { true, "CreateTest", "Column1" });
+		create.columns("SID", "int", false, true, false, { true, "CreateTest", "Column2" });
+		REQUIRE(create.str() == "create table CreateModel(UFID int not null, SID int not null, foreign key (UFID) references CreateTest(Column1), foreign key (SID) references CreateTest(Column2))");
+	}
+
+	SECTION("Reset after lots of operations") {
+		create.columns("UFID", "int", true, true, false, { true, "CreateTest", "Column1" });
+		create.columns("SID", "int", false, true, false, { true, "CreateTest", "Column2" });
+		create.columns("FName", "varchar(255)", false, true);
+		create.columns("LName", "varchar(255)", false, true);
+		REQUIRE(create.str() == "create table CreateModel(UFID int not null, SID int not null, FName varchar(255) not null, LName varchar(255) not null, primary key(UFID), foreign key (UFID) references CreateTest(Column1), foreign key (SID) references CreateTest(Column2))");
+		create.reset();
+		REQUIRE(create.str() == "");
+	}
+}
+
+
+TEST_CASE("DropModel", "[sqlgenerator::DropModel]") {
+	sqlGenerator::DropModel drop;
+	REQUIRE(drop.str() == "");
+	drop.tableName("DropModel");
+	REQUIRE(drop.str() == "drop table DropModel");
+	drop.reset();
+	REQUIRE(drop.str() == "");
+}
+
